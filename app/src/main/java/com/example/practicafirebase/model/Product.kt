6f9fd183fb74da203a0fdd965550593b985170ce
Ester Castellano Ríos <estercastellanorios@gmail.com>
2026-02-03 20:40:48 +0100

    Conexion con la base dde datos Firebase y metodos CRUD

---
+++ app/src/main/java/com/example/practicafirebase/model/Product.kt
@@ -0,0 +1,7 @@
+package com.example.practicafirebase.model
+
+data class Product(
+    var id: String = "",
+    val name: String = "",
+    val price: Double = 0.0,
+)
---
+++ app/src/main/java/com/example/practicafirebase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.practicafirebase.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.practicafirebase.model.Product
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeViewModel: ViewModel() {
+    private val db = Firebase.firestore
+    private val productsCollection = db.collection("productos")
+
+    private val _products = MutableStateFlow<List<Product>>(emptyList())
+    val products: StateFlow<List<Product>> = _products
+
+    init {
+        getProducts()
+    }
+
+    private fun getProducts() {
+        productsCollection.addSnapshotListener { snapshot, error ->
+            if (error != null){
+                return@addSnapshotListener
+            }
+            if (snapshot != null){
+                val productsList = snapshot.documents.mapNotNull { doc ->
+                    val product = doc.toObject(Product::class.java)
+                    product?.id = doc.id
+                    product
+                }
+                _products.value = productsList
+            }
+        }
+    }
+
+    private fun addProduct(name: String, price: Double) {
+        val product = Product(name = name, price = price)
+        productsCollection.add(product)
+            .addOnFailureListener { e ->
+                Log.e("Error Firebase", "Error al guardar: ${e.message}", e)
+            }
+            .addOnSuccessListener {  }
+            .addOnCompleteListener {  }
+    }
+
+    private fun deleteProduct(id: String) {
+        productsCollection.document(id)
+            .delete()
+            .addOnFailureListener {
+                Log.i("Firebase", "Borrado correctamente")
+            }
+            .addOnFailureListener { e ->
+                Log.e("Error Firebase", "Error al eliminar: ${e.message}", e)
+            }
+    }
+
+    private fun updateProduct(id: String, name: String, price: Double?){
+        val data = mutableMapOf<String, Any>()
+
+        if (name.isNotEmpty() && name.isNotBlank()){
+            data["nombre"] = name
+        }
+        if (price != null){
+            data["precio"] = price
+        }
+
+        productsCollection.document(id)
+            .update(data)
+            .addOnSuccessListener {
+                Log.i("Firebase", "Actualizado correctamente")
+            }
+            .addOnFailureListener { e ->
+                Log.e("Error Firebase", "Error al actualizar: ${e.message}", e)
+            }
+    }
+}
